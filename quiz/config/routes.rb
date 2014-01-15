79877cb5ddfec0034274e4a66ac4afdb2c40e569
astux7 <Asta.bevainyte@gmail.com>
2014-01-15 18:06:00 +0000

    added routes

--- quiz/config/routes.rb
+++ quiz/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  #devise_for :users
+  root "questions#index"
+  resources :question 
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
