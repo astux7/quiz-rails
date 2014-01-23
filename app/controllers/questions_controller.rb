57544e264b37f4cf89a9c84a157bd79cb33a9411
astux7 <Asta.bevainyte@gmail.com>
2014-01-23 14:27:47 +0000

    add app

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,47 @@
+class QuestionsController < ApplicationController
+
+  
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new params[:question].permit(:description, :correct_answer)
+    @question.user = current_user
+    if @question.save
+      redirect_to questions_path
+    else
+      render 'new'
+    end
+  end
+ 
+  def show
+    @question = Question.find params[:id] 
+  end
+
+  def quiz
+    @questions = Question.where("user_id <> ?", current_user)
+    @question = @questions[0]
+  end
+
+  def index
+    @questions = Question.where("user_id = ?", current_user)
+  end
+
+  def edit
+    @question = Question.find params[:id]
+  end
+
+  def update
+    @question = Question.find params[:id]
+    @question.update params[:question].permit(:description, :correct_answer)
+    redirect_to questions_path
+  end
+
+  def destroy
+    @question = Question.find params[:id]
+    @question.destroy
+    redirect_to questions_path
+  end
+
+end
---
+++ app/helpers/questions_helper.rb
@@ -0,0 +1,3 @@
+module QuestionsHelper
+
+end
